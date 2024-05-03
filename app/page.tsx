f346bbe10ec22e1b53e0ac0f936ed23d4b3d7cc9
Владислав <pahnetstarushkami@bk.ru>
2024-05-03 15:24:13 +0300

    Все новое

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
 'use client'
 
-import {Title} from '@/components/custom/HeadingName'
+
 import { FloatingNavDemo } from '@/components/custom/Navbar';
 import { ChakraProvider } from '@chakra-ui/react';
-import  HeaderSection  from '@/components/MyComponent/HeroSection'
-import  TestimonialCard  from '@/components/ui/Testimonials'
+import  Header  from '@/components/Header'
+ 
 
 
 export default function Home() {
   return (
     <>
-    <FloatingNavDemo/>
-    <ChakraProvider>
-    <HeaderSection/>
-    <TestimonialCard/>
-    </ChakraProvider>
+    <FloatingNavDemo/> 
     
+    <ChakraProvider>
+    <Header/>
+    </ChakraProvider>
     </>
   );
 }
