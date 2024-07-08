d2d46b4c75973072c6d9a0c3bd527049ceee6389
Владислав <pahnetstarushkami@bk.ru>
2024-07-08 19:10:04 +0300

    2ef

--- components/Prices.jsx
+++ components/Prices.jsx
@@ -38,237 +38,243 @@ const PriceList = () => {
       justifyContent="center"
       alignItems="center"
     >
-      <Box w="70em" pt={8}>
-        <Flex
-          direction={{
-            base: "column",
-            md: "row",
-          }}
-          justifyContent="center"
-          mb={{
-            base: 6,
-            sm: 0,
-          }}
-        >
-          {/* Hobby Plan */}
+      <motion.div
+        ref={ref}
+        style={{
+          scale: scaleProgress,
+          opacity: opacityProgress,
+        }}
+      >
+        <Box w="70em" pt={8}>
           <Flex
-            flex={{
-              sm: 1,
-              lg: "initial",
+            direction={{
+              base: "column",
+              md: "row",
             }}
-            w={{
-              lg: 2.3 / 7,
+            justifyContent="center"
+            mb={{
+              base: 6,
+              sm: 0,
             }}
-            rounded="35px"
-            bg="#193CF9"
-            my={6}
-            direction="column"
-            className="shadow-md hover:shadow-lg hover:translate-x-2 hover:scale-105 transition-all duration-300"
           >
-            <VStack
-              spacing={1}
-              justifyContent="center"
-              p={8}
-              textAlign="center"
-              w="full"
-              shadow="xl"
-            >
-              <chakra.span fontSize="4xl" fontWeight="bold" color={"white"}>
-                🎨 Хобби
-              </chakra.span>
-
-              <chakra.span fontSize="5xl" fontWeight={"bold"} color={"white"}>
-                4800 ₽
-              </chakra.span>
-              <chakra.span
-                alignSelf="center"
-                fontSize="2xl"
-                _light={{
-                  color: "white",
-                }}
-              >
-                /месяц
-              </chakra.span>
-            </VStack>
-            <VStack
-              borderBottomLeftRadius="35px"
-              borderBottomRightRadius="35px"
-              fontSize="sm"
-              spacing={8}
-              h="full"
-              bg="gray.50"
-              _dark={{
-                bg: "gray.800",
+            {/* Hobby Plan */}
+            <Flex
+              flex={{
+                sm: 1,
+                lg: "initial",
               }}
-              p={12}
+              w={{
+                lg: 2.3 / 7,
+              }}
+              rounded="35px"
+              bg="#193CF9"
+              my={6}
+              direction="column"
+              className="shadow-md hover:shadow-lg hover:translate-x-2 hover:scale-105 transition-all duration-300"
             >
               <VStack
-                spacing={4}
+                spacing={1}
+                justifyContent="center"
+                p={8}
+                textAlign="center"
                 w="full"
-                direction="column"
-                alignItems="start"
+                shadow="xl"
               >
-                <Feature>Абонемент на 1 месяц</Feature>
-                <Feature>Регулярные занятия</Feature>
-                <Feature>Предварительная запись обязательна</Feature>
+                <chakra.span fontSize="4xl" fontWeight="bold" color={"white"}>
+                  🎨 Хобби
+                </chakra.span>
+
+                <chakra.span fontSize="5xl" fontWeight={"bold"} color={"white"}>
+                  4800 ₽
+                </chakra.span>
+                <chakra.span
+                  alignSelf="center"
+                  fontSize="2xl"
+                  _light={{
+                    color: "white",
+                  }}
+                >
+                  /месяц
+                </chakra.span>
               </VStack>
-              <Box
-                w="full"
-                ml={3}
-                display="inline-flex"
-                rounded="35px"
-                shadow="md"
-              ></Box>
-            </VStack>
-          </Flex>
-
-          <Flex
-            flex={{
-              base: 1,
-              lg: "initial",
-            }}
-            w={{
-              lg: 2.4 / 7,
-            }}
-            rounded="35px"
-            bg="blue.800"
-            mt={{
-              base: 4,
-              sm: -4,
-            }}
-            shadow="md"
-            zIndex={30}
-            direction="column"
-            className="shadow-md hover:shadow-lg hover:translate-x-2 hover:scale-105 transition-all duration-300"
-          >
-            <VStack
-              spacing={1}
-              justifyContent="center"
-              p={8}
-              textAlign="center"
-              w="full"
-              shadow="xl"
-            >
-              <chakra.span fontSize="4xl" fontWeight="bold" color={"white"}>
-                🚀 Развитие
-              </chakra.span>
-
-              <chakra.span
-                fontSize="5xl"
-                as="u"
-                px={4}
-                fontWeight={"bold"}
-                color={"white"}
-              >
-                8800 ₽
-              </chakra.span>
-              <chakra.span
-                alignSelf="center"
-                fontSize="2xl"
-                _light={{
-                  color: "white",
+              <VStack
+                borderBottomLeftRadius="35px"
+                borderBottomRightRadius="35px"
+                fontSize="sm"
+                spacing={8}
+                h="full"
+                bg="gray.50"
+                _dark={{
+                  bg: "gray.800",
                 }}
+                p={12}
               >
-                /2 месяца
-              </chakra.span>
-            </VStack>
-            <VStack
-              borderBottomLeftRadius="35px"
-              borderBottomRightRadius="35px"
-              fontSize="sm"
-              h="full"
-              spacing={8}
-              bg="gray.50"
-              _dark={{
-                bg: "gray.800",
+                <VStack
+                  spacing={4}
+                  w="full"
+                  direction="column"
+                  alignItems="start"
+                >
+                  <Feature>Абонемент на 1 месяц</Feature>
+                  <Feature>Регулярные занятия</Feature>
+                  <Feature>Предварительная запись обязательна</Feature>
+                </VStack>
+                <Box
+                  w="full"
+                  ml={3}
+                  display="inline-flex"
+                  rounded="35px"
+                  shadow="md"
+                ></Box>
+              </VStack>
+            </Flex>{" "}
+            <Flex
+              flex={{
+                base: 1,
+                lg: "initial",
               }}
-              p={12}
+              w={{
+                lg: 2.4 / 7,
+              }}
+              rounded="35px"
+              bg="blue.800"
+              mt={{
+                base: 4,
+                sm: -4,
+              }}
+              shadow="md"
+              zIndex={30}
+              direction="column"
+              className="shadow-md hover:shadow-lg hover:translate-x-2 hover:scale-105 transition-all duration-300"
             >
               <VStack
-                spacing={4}
+                spacing={1}
+                justifyContent="center"
+                p={8}
+                textAlign="center"
                 w="full"
-                direction="column"
-                alignItems="start"
+                shadow="xl"
               >
-                <Feature>Абонемент на 2 месяца</Feature>
-                <Feature>Регулярные занятия </Feature>
-                <Feature>Предварительная запись обязательна</Feature>
+                <chakra.span fontSize="4xl" fontWeight="bold" color={"white"}>
+                  🚀 Развитие
+                </chakra.span>
+
+                <chakra.span
+                  fontSize="5xl"
+                  as="u"
+                  px={4}
+                  fontWeight={"bold"}
+                  color={"white"}
+                >
+                  8800 ₽
+                </chakra.span>
+                <chakra.span
+                  alignSelf="center"
+                  fontSize="2xl"
+                  _light={{
+                    color: "white",
+                  }}
+                >
+                  /2 месяца
+                </chakra.span>
               </VStack>
-            </VStack>
-          </Flex>
-
-          {/* Scale Plan */}
-          <Flex
-            flex={{
-              sm: 1,
-              lg: "initial",
-            }}
-            w={{
-              lg: 2.3 / 7,
-            }}
-            roundedTop="35px"
-            bg="#115CF2"
-            _dark={{
-              bg: "gray.700",
-            }}
-            my={6}
-            direction="column"
-            className="shadow-md hover:shadow-lg hover:translate-x-2 hover:scale-105 transition-all duration-300"
-            borderBottomLeftRadius="35px"
-            borderBottomRightRadius="35px"
-          >
-            <VStack
-              spacing={1}
-              justifyContent="center"
-              p={8}
-              textAlign="center"
-              w="full"
-              shadow="xl"
-            >
-              <chakra.span fontSize="3xl" fontWeight="bold" color={"white"}>
-                😉 Попробовать
-              </chakra.span>
-
-              <chakra.span fontSize="5xl" fontWeight={"bold"} color={"white"}>
-                500 ₽
-              </chakra.span>
-              <chakra.span
-                alignSelf="center"
-                fontSize="2xl"
-                _light={{
-                  color: "white",
+              <VStack
+                borderBottomLeftRadius="35px"
+                borderBottomRightRadius="35px"
+                fontSize="sm"
+                h="full"
+                spacing={8}
+                bg="gray.50"
+                _dark={{
+                  bg: "gray.800",
                 }}
+                p={12}
               >
-                /занятие
-              </chakra.span>
-            </VStack>
-            <VStack
-              fontSize="sm"
-              spacing={8}
-              h="full"
-              bg="gray.50"
-              _dark={{
-                bg: "gray.800",
+                <VStack
+                  spacing={4}
+                  w="full"
+                  direction="column"
+                  alignItems="start"
+                >
+                  <Feature>Абонемент на 2 месяца</Feature>
+                  <Feature>Регулярные занятия </Feature>
+                  <Feature>Предварительная запись обязательна</Feature>
+                </VStack>
+              </VStack>
+            </Flex>
+            {/* Scale Plan */}
+            <Flex
+              flex={{
+                sm: 1,
+                lg: "initial",
               }}
+              w={{
+                lg: 2.3 / 7,
+              }}
+              roundedTop="35px"
+              bg="#115CF2"
+              _dark={{
+                bg: "gray.700",
+              }}
+              my={6}
+              direction="column"
+              className="shadow-md hover:shadow-lg hover:translate-x-2 hover:scale-105 transition-all duration-300"
               borderBottomLeftRadius="35px"
               borderBottomRightRadius="35px"
-              p={12}
             >
               <VStack
-                spacing={4}
+                spacing={1}
+                justifyContent="center"
+                p={8}
+                textAlign="center"
                 w="full"
-                direction="column"
-                alignItems="start"
+                shadow="xl"
               >
-                <Feature>
-                  Если ребенку понравилось на пробном занятии и он будет дальше
-                  учиться, то данное занятие для вас БЕСПЛАТНО.
-                </Feature>
+                <chakra.span fontSize="3xl" fontWeight="bold" color={"white"}>
+                  😉 Попробовать
+                </chakra.span>
+
+                <chakra.span fontSize="5xl" fontWeight={"bold"} color={"white"}>
+                  500 ₽
+                </chakra.span>
+                <chakra.span
+                  alignSelf="center"
+                  fontSize="2xl"
+                  _light={{
+                    color: "white",
+                  }}
+                >
+                  /занятие
+                </chakra.span>
               </VStack>
-            </VStack>
+              <VStack
+                fontSize="sm"
+                spacing={8}
+                h="full"
+                bg="gray.50"
+                _dark={{
+                  bg: "gray.800",
+                }}
+                borderBottomLeftRadius="35px"
+                borderBottomRightRadius="35px"
+                p={12}
+              >
+                <VStack
+                  spacing={4}
+                  w="full"
+                  direction="column"
+                  alignItems="start"
+                >
+                  <Feature>
+                    Если ребенку понравилось на пробном занятии и он будет
+                    дальше учиться, то данное занятие для вас БЕСПЛАТНО.
+                  </Feature>
+                </VStack>
+              </VStack>
+            </Flex>
           </Flex>
-        </Flex>
-      </Box>
+        </Box>
+      </motion.div>
     </Flex>
   );
 };
